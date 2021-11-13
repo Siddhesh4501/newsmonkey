e2e8bb42c56e968c7ec3037e2cb6a953d127c093
Siddhesh4501 <pawarsd20.comp@coep.ac.in>
2021-11-13 16:29:34 +0530

    yes

--- src/App.js
+++ src/App.js
@@ -20,23 +20,23 @@ const App =()=> {
         <Switch>
 
 
-        <Route exact path="/">
+        <Route exact path="/newsmonkey">
         {/* <Route exact path={process.env.PUBLIC_URL + '/'}> */}
             <News key="general" pagesize={9} contenttype="general"/>
         </Route>
-        <Route exact path="/business">
+        <Route exact path="/newsmonkey/business">
             <News key="business" pagesize={9} contenttype="business"/>
         </Route>
-        <Route exact path="/entertainment">
+        <Route exact path="/newsmonkey/entertainment">
             <News key="entertainment" pagesize={9} contenttype="entertainment"/>
         </Route>
-        <Route exact path="/science">
+        <Route exact path="/newsmonkey/science">
             <News key="science" pagesize={9} contenttype="science"/>
         </Route>
-        <Route exact path="/sports">
+        <Route exact path="/newsmonkey/sports">
             <News key="sports" pagesize={9} contenttype="sports"/>
         </Route>
-        <Route exact path="/technology">
+        <Route exact path="/newsmonkey/technology">
             <News key="technology" pagesize={9} contenttype="technology"/>
         </Route>
 
